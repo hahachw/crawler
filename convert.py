6e3120bc86a7c71475e287134aad0da26a311f7f
hahachw <hahahchw@kaist.ac.kr>
2025-07-19 02:39:31 +0900

    analyzed data

--- convert.py
+++ convert.py
@@ -1,15 +1,33 @@
 import json
 
-with open("data.json", "r", encoding="utf-8") as f:
-    data_list = json.load(f)
+def convert_for_text_fluoroscopy():
+    with open("data.json", "r", encoding="utf-8") as f:
+        data_list = json.load(f)
 
-converted_list = []
-for post in data_list:
-    converted = {
-        "text" : post["content"],
-        "result" : 0
+    converted_list = []
+    for post in data_list:
+        converted = {
+            "text" : post["content"],
+            "result" : 0
+        }
+        converted_list.append(converted)
+
+    with open("naver_blog.json", "w", encoding="utf-8") as f:
+        json.dump(converted_list, f, ensure_ascii=False, indent=4)
+
+def convert_for_ReMoDetect():
+    with open("data.json", "r", encoding="utf-8") as f:
+        data_list = json.load(f)
+    
+    texts = [post["content"] for post in data_list]
+
+    converted_list = {
+        "original" : texts,
+        "sampled" : texts.copy()
     }
-    converted_list.append(converted)
 
-with open("naver_blog.json", "w", encoding="utf-8") as f:
-    json.dump(converted_list, f, ensure_ascii=False, indent=4)
+    with open("naver_naver_blog.raw_data.json", "w", encoding="utf-8") as f:
+        json.dump(converted_list, f, ensure_ascii=False, indent=4)
+
+convert_for_text_fluoroscopy()
+convert_for_ReMoDetect()
